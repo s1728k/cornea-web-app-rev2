066b65d65d11af8f596222d0f96470aa3490882c
arun bangar <arun.bangar@codebakerz.in>
2017-05-29 15:26:14 +0530

    added file upload component

---
+++ corena/src/app/file-upload/file-upload.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+import {FileUploader} from 'ng2-file-upload';
+
+const URL = 'https://evening-anchorage-3159.herokuapp.com/api/';
+
+@Component({
+  selector: 'app-file-upload',
+  templateUrl: './file-upload.component.html',
+  styleUrls: ['./file-upload.component.css']
+})
+export class FileUploadComponent implements OnInit {
+  public uploader: FileUploader = new FileUploader({url: URL});
+  public hasBaseDropZoneOver = false;
+  public hasAnotherDropZoneOver = false;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
