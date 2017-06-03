7738499e538d9ca033c5389c9c917e7f221dfd59
sunilkumarcodebakerz <sunilkumarcodebakerz@codebakerz.in>
2017-06-03 21:55:09 +0530

    ui functionality for rate analysis to add delete tables and rows

--- corena/src/app/rate-analysis/rate-analysis.component.ts
+++ corena/src/app/rate-analysis/rate-analysis.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RateAnalysis } from '../model/rate.model'
 
 @Component({
   selector: 'app-rate-analysis',
@@ -7,11 +8,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RateAnalysisComponent implements OnInit {
 
-  calcs:{}[]=[{},{},{},{},{},{},{},{},{},{}]
+  calcs1:RateAnalysis[][]=[];
+  calcs2:RateAnalysis[][]=[];
+  calcs3:RateAnalysis[][]=[];
 
   constructor() { }
 
   ngOnInit() {
+
   }
 
+  // overhead:number;
+  // tff(val){
+  //   this.overhead = this.overhead
+  // }
+  //list:any[]=[]
+  increment1(){
+    this.calcs1.push([])
+  }
+  deleteTable1(i){
+     this.calcs1.splice(i, 1);
+  }
+  addRow1(i){
+    this.calcs1[i].push({})
+  }
+  deleteRow1(i,j){
+    this.calcs1[i].splice(j, 1);
+  }
+
+
+  increment2(){
+    this.calcs2.push([])
+  }
+  deleteTable2(i){
+     this.calcs2.splice(i, 1);
+  }
+  addRow2(i){
+    this.calcs2[i].push({})
+  }
+  deleteRow2(i,j){
+    this.calcs2[i].splice(j, 1);
+  }
+
+
+  increment3(){
+    this.calcs3.push([])
+  }
+  deleteTable3(i){
+     this.calcs3.splice(i, 1);
+  }
+  addRow3(i){
+    this.calcs3[i].push({})
+  }
+  deleteRow3(i,j){
+    this.calcs3[i].splice(j, 1);
+  }
 }
