b2c07efc055658bb6be19f69c379b87b93184b3e
sunilkumarcodebakerz <sunilkumarcodebakerz@codebakerz.in>
2017-06-04 03:22:51 +0530

    add grand total and description dropdown

--- corena/src/app/rate-analysis/rate-analysis.component.ts
+++ corena/src/app/rate-analysis/rate-analysis.component.ts
@@ -36,6 +36,14 @@ export class RateAnalysisComponent implements OnInit {
   deleteRow1(i,j){
     this.calcs1[i].splice(j, 1);
   }
+  grandTotalV:number=0;
+  grandTotal(j){
+    this.grandTotalV=0;
+    for (var i = this.calcs1[j].length - 1; i >= 0; i--) {
+      this.grandTotalV=this.grandTotalV+(+this.calcs1[j][i]['v1'])*(+this.calcs1[j][i]['v2'])* (+this.calcs1[j][i]['v3'])*0.0012*8000* (+this.calcs1[j][i]['v4'])
+    }
+  }
+  dropList1:{}[]=[{"key":"value1"},{"key":"value2"},{"key":"value3"},{"key":"value4"}]
 
 
   // increment2(){
@@ -51,6 +59,15 @@ export class RateAnalysisComponent implements OnInit {
     this.calcs2.splice(j, 1);
   }
 
+  grandTotal2V:number=0;
+  grandTotal2(){
+    this.grandTotal2V=0;
+    for (var i = this.calcs2.length - 1; i >= 0; i--) {
+      this.grandTotal2V=this.grandTotal2V+(+this.calcs2[i]['v1'])*(+this.calcs2[i]['v2'])* (+this.calcs2[i]['v3'])*0.0012*8000* (+this.calcs2[i]['v4'])
+    }
+  }
+  dropList2:{}[]=[{"key":"value1"},{"key":"value2"},{"key":"value3"},{"key":"value4"}]
+
 
   // increment3(){
   //   this.calcs3.push([])
@@ -64,4 +81,6 @@ export class RateAnalysisComponent implements OnInit {
   // deleteRow3(i,j){
   //   this.calcs3[i].splice(j, 1);
   // }
+
+
 }
