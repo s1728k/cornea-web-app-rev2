209b206060c2ebd8687749dcabe51551740bdde5
Ronney <ronneyismaelcodebakerz>
2017-06-08 18:12:54 +0530

    Implemented Material Design in ganttchart.component.html

--- corena/src/app/shared/components/ganttchart/ganttchart.component.ts
+++ corena/src/app/shared/components/ganttchart/ganttchart.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Project, IGanttOptions, Zooming, Task } from '../../../lib';
+import {Component} from '@angular/core';
+import {Project, IGanttOptions, Zooming, Task} from '../../../lib';
 
 @Component({
   selector: 'app-ganttchart',
@@ -18,15 +18,15 @@ export class GanttchartComponent {
   };
 
   project: Project = {
-    'id': 'dd10f0b6-b8a4-4b2d-a7df-b2c3d63b4a01',
+    'id': '001',
     'name': 'Angular2 Gantt',
     'startDate': new Date('2017-02-27T08:32:09.6972999Z'),
     'tasks': [
       {
-        'id': 'ea2a8d86-1d4b-4807-844d-d5417fcf618d',
-        'treePath': 'parent 1',
+        'id': '002',
+        'treePath': 'Planning',
         'parentId': 'ea2a8d86-1d4b-4807-844d-d5417fcf618d',
-        'name': 'parent 1',
+        'name': 'Planning',
         'resource': 'res1',
         'start': new Date('2017-01-01T00:00:00.0Z'),
         'end': new Date('2017-01-03T00:00:00.0Z'),
@@ -34,30 +34,30 @@ export class GanttchartComponent {
         'status': 'Completed'
       },
       {
-        'id': 'dd755f20-360a-451f-b200-b83b89a35ad1',
-        'treePath': 'Cras sollicitudin egestas velit sit amet aliquam',
+        'id': '003',
+        'treePath': 'Research',
         'parentId': 'dd755f20-360a-451f-b200-b83b89a35ad1',
-        'name': 'Cras sollicitudin egestas velit sit amet aliquam',
+        'name': 'Research',
         'resource': 'res2',
         'start': new Date('2017-01-05T00:00:00.0Z'),
         'end': new Date('2017-01-06T00:00:00.0Z'),
         'percentComplete': 0
       },
       {
-        'id': 'j1b997ef-bb89-4ca2-b134-62a08a19aef6',
-        'treePath': 'Donec ac augue est',
+        'id': '004',
+        'treePath': 'Implementation',
         'parentId': 'j1b997ef-bb89-4ca2-b134-62a08a19aef6',
-        'name': 'Donec ac augue est',
+        'name': 'Implementation',
         'resource': 'res2',
         'start': new Date('2017-01-06T00:00:00.0Z'),
         'end': new Date('2017-01-07T00:00:00.0Z'),
         'percentComplete': 0
       },
       {
-        'id': 'ub12f674-d5cb-408f-a941-ec76af2ec47e',
-        'treePath': 'Lorem ipsum dolor sit amet',
+        'id': '005',
+        'treePath': 'Testing',
         'parentId': 'ub12f674-d5cb-408f-a941-ec76af2ec47e',
-        'name': 'Lorem ipsum dolor sit amet',
+        'name': 'Testing',
         'resource': 'res1',
         'start': new Date('2017-01-07T00:00:00.0Z'),
         'end': new Date('2017-01-22T00:00:00.0Z'),
@@ -65,44 +65,25 @@ export class GanttchartComponent {
         'status': 'Error'
       },
       {
-        'id': 'xafa430b-d4da-4d7d-90ed-69056a042d7a',
-        'treePath': 'Praesent molestie lobortis mi non tempor',
+        'id': '006',
+        'treePath': 'Deployment',
         'parentId': 'xafa430b-d4da-4d7d-90ed-69056a042d7a',
-        'name': 'Praesent molestie lobortis mi non tempor',
+        'name': 'Deployment',
         'resource': 'res1',
         'start': new Date('2017-01-22T00:00:00.0Z'),
         'end': new Date('2017-01-23T00:00:00.0Z')
       },
       {
-        'id': 'mc9d8d41-1995-4b38-9256-bcc0da171146',
-        'treePath': 'Cras sollicitudin egestas velit sit amet aliquam',
+        'id': '007',
+        'treePath': 'Delivery',
         'parentId': 'mc9d8d41-1995-4b38-9256-bcc0da171146',
-        'name': 'Cras sollicitudin egestas velit sit amet aliquam',
+        'name': 'Delivery',
         'resource': 'res2',
         'start': new Date('2017-01-23T00:00:00.0Z'),
         'end': new Date('2017-01-24T00:00:00.0Z')
-      },
-      {
-        'id': 'b5c071a5-430c-4d61-acf4-799cbdf61c49',
-        'treePath': 'Donec ac augue est',
-        'parentId': 'b5c071a5-430c-4d61-acf4-799cbdf61c49',
-        'name': 'Donec ac augue est',
-        'resource': 'res2',
-        'start': new Date('2017-01-24T00:00:00.0Z'),
-        'end': new Date('2017-01-24T00:34:00.0Z')
-      },
-      {
-        'id': 't9ba762e-bde7-47bf-b628-75f99fdd5bef',
-        'treePath': 'Lorem Ipsum',
-        'parentId': 't9ba762e-bde7-47bf-b628-75f99fdd5bef',
-        'name': 'Lorem Ipsum',
-        'resource': 'res2',
-        'start': new Date('2017-01-24T00:00:00.0Z'),
-        'end': new Date('2017-01-24T00:00:36.0Z')
       }
     ]
   };
-
 
 
   constructor() {
@@ -122,6 +103,11 @@ export class GanttchartComponent {
     });
   }
 
+  /* This Method is used to create a new task with a id generated,name generated,
+   tree path generated and percentage complete generated as 0%
+   by default also start date and end date is also generated .
+   We can change the fields as it is two way binded.
+   */
   createTask(element: any) {
     const selectedStatus = element.options[element.selectedIndex].value;
 
@@ -134,7 +120,7 @@ export class GanttchartComponent {
       'start': new Date('2017-01-01T03:30:00.0Z'),
       'end': new Date('2017-01-01T12:45:00.0Z'),
       'status': selectedStatus
-    }
+    };
     this.project.tasks.push(parentTask);
 
     const childTask = {
@@ -146,26 +132,27 @@ export class GanttchartComponent {
       'start': new Date('2017-01-01T03:30:00.0Z'),
       'end': new Date('2017-01-01T12:45:00.0Z'),
       'status': selectedStatus
-    }
+    };
     this.project.tasks.push(childTask);
 
   }
 
+  // When you click this button the task will be updated till 100%.
   updateTasks() {
     for (let i = 0; i < this.project.tasks.length; i++) {
       let task = this.project.tasks[i];
 
       let progress = setInterval(function () {
         if (task.percentComplete === 100) {
-          task.status = "Completed";
+          task.status = 'Completed';
           clearInterval(progress);
         } else {
           if (task.percentComplete === 25) {
-            task.status = "Warning";
+            task.status = 'Warning';
           } else if (task.percentComplete === 50) {
-            task.status = "Error";
+            task.status = 'Error';
           } else if (task.percentComplete === 75) {
-            task.status = "Information";
+            task.status = 'Information';
           }
 
           task.percentComplete += 1;
@@ -174,6 +161,9 @@ export class GanttchartComponent {
     }
   }
 
+  /* This method is used to generate large number of big dataset that will be loaded based on the
+   limits set on it.Here it is set upto 1000
+   */
   loadBigDataSet() {
     var tasks = [];
 
@@ -185,7 +175,7 @@ export class GanttchartComponent {
         start: new Date(),
         end: new Date(),
         status: ''
-      }
+      };
 
       tasks.push(task);
     }
