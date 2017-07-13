986ac4240aee852fcb67fbac16d71b2cfc9f7119
Ronney <ronneyismaelcodebakerz>
2017-07-13 18:01:02 +0530

    Added dialogs in Task Management Component 2

--- corena/src/app/taskmanagement/taskmanagement.component.ts
+++ corena/src/app/taskmanagement/taskmanagement.component.ts
@@ -153,7 +153,7 @@ export class TaskmanagementComponent implements OnInit, OnChanges {
   }
 
   showChildTask(parent_task_id, child_task_id){
-    this.mdDialog.open(ShowSubtaskDialogComponent);
+    this.mdDialog.open(ShowSubtaskDialogComponent, {data: child_task_id});
     // alert("Show Child Task Popup Shall Come Soon \n Task Id: " + parent_task_id.id + " Sub Task Id: " + child_task_id.id);
   }
 
